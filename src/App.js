3ec8d94ae7e246c6f0222ce8e2acd4a809606e16
domosweb <domos.offical@gmail.com>
2025-03-20 15:43:22 +0100

    we started with home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Shop from "./pages/Shop";
+import Products from "./pages/Products";
+import Contactus from "./pages/Contactus";
+
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,10 +19,15 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/contactus" element={<Contactus />} />
 
     </Routes>
   </Router>
   );
 }
+
+
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,41 @@ const Home = () => {
   <div className='home-banner'>
 
   </div>
+
+  <body>
+  <header className="homeheader">
+    <a href="#" className='logo'>Domos</a>
+
+
+
+    <nav className='navbar'>
+      <a href="#" className='active'>Home</a>
+      <a href="#">About</a>
+      <a href="#">Shop</a>
+      <a href="#">Products</a>
+      <a href="#">Contact us</a>
+      <a href="#">WebApp</a>
+      
+    </nav>
+</header>
+
+<section className='home'>
+<div className='home-content'>
+  <h1>Domos</h1>
+  <h3>Your home,your Domos</h3>
+  <p>gtg</p>
+  <div className='btn-box'>
+    <a href='#'>Hire me</a>
+    <a href='#'>Let's talk</a>
+
+
+
+
+    
+  </div>
+</div>
+</section>
+</body>
   
   </>);
 };
