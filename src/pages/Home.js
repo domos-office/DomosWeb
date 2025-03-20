86f6c45a097aa5bdd5174f19171540c9a8007ac7
domosweb <domos.offical@gmail.com>
2025-03-20 14:29:57 +0100

    ardita added a banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Home = () => {
-  return <h1>Welcome to Home Page</h1>;
+  return(<>
+    
+  <h1>Welcome to Home Page</h1>
+  <div className='home-banner'>
+
+  </div>
+  
+  </>);
 };
 
 export default Home;
