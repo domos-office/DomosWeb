cbe7518e9bc95bbbc06d611e41f725c543f3cf61
Endri Ndoja <endndo21@htl-shkoder.com>
2025-03-20 15:44:46 +0100

    test4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Webappdashboard from "./pages/Webappdashboard";
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,12 +10,12 @@ function App() {
   return (
     <Router>
     <nav>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/login">Login</Link>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/login">Login</Link> | <Link to="/webappdashboard">WebApp</Link>
     </nav>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-
+      <Route path="/webappdashboard" element={<Webappdashboard />} />
     </Routes>
   </Router>
   );
