04fbd74c83fe24d9df454924c92d63869e2209c7
Endri Ndoja <endndo21@htl-shkoder.com>
2025-03-20 15:52:31 +0100

    added webapp

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-<<<<<<< HEAD
-import Webappdashboard from "./pages/Webappdashboard";
-=======
 import Shop from "./pages/Shop";
 import Products from "./pages/Products";
 import Contactus from "./pages/Contactus";
-
-
->>>>>>> 3ec8d94ae7e246c6f0222ce8e2acd4a809606e16
+import Webapp from "./pages/Webapp";
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,19 +13,15 @@ function App() {
   return (
     <Router>
     <nav>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/login">Login</Link> | <Link to="/webappdashboard">WebApp</Link>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/login">Login</Link> | <Link to="/webapp">WebApp</Link>
     </nav>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-<<<<<<< HEAD
-      <Route path="/webappdashboard" element={<Webappdashboard />} />
-=======
       <Route path="/shop" element={<Shop />} />
       <Route path="/products" element={<Products />} />
       <Route path="/contactus" element={<Contactus />} />
-
->>>>>>> 3ec8d94ae7e246c6f0222ce8e2acd4a809606e16
+      <Route path="/webapp" element={<Webapp />} />
     </Routes>
   </Router>
   );
