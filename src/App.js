db01cad2ee7fd5270d5a698c3f352ae93b5b739b
Endri Ndoja <endndo21@htl-shkoder.com>
2025-03-20 14:05:38 +0100

    Test3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      
-      </header>
-    </div>
+    <Router>
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/login">Login</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+
+    </Routes>
+  </Router>
   );
 }
 
