9709a29f54d162d56fe7ac92dfab4e31b3a7c678
domosweb <domos.offical@gmail.com>
2025-04-13 18:22:53 +0200

    punova pak me about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import '../App.css';
+import aboutImage from '../assets/about-illustration.jpg';
 
-const Home = () => {
-  return <h1>DOMOS</h1>;
+const About = () => {
+  return (
+    <div className="about-section">
+      <div className="about-text">
+        <h1>About us</h1>
+        <p>
+          At Domos, our mission is to redefine modern living through intelligent,
+          connected home experiences. We aim to empower users with technology that’s
+          simple, secure, and seamlessly integrated into everyday life.
+        </p>
+        <button className="learn-more-button">Learn More</button>
+      </div>
+      <div className="about-image">
+        <img src={aboutImage} alt="Smart home illustration" />
+      </div>
+    </div>
+  );
+  
 };
 
-export default Home;
+export default About;
+
